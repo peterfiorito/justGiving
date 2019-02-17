f51d814bddb589a9117c08a842145015c12f7262
Peter <pedrojfiorito@gmail.com>
2019-02-18 00:06:33 +0100

    Increase axios timeout

--- src/services/apiRequestService.js
+++ src/services/apiRequestService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'http://localhost:8081',
-    timeout: 1000
+    timeout: 4000
 });
 
 export default class generalApiRequestService {
